15422bb3e5ee14e6377e19cbd8d179ef4512816a
Akash-Potti <akashpotti07@gmail.com>
2025-11-07 08:30:24 +0530

    movie tags

---
+++ Movie/tags_mapper.py
@@ -0,0 +1,15 @@
+import sys
+import csv
+
+for line in sys.stdin:
+	line=line.strip()
+	if not line or line.startswith("userId"):
+		continue
+	try:
+		fields=line.split(",")
+		movieId=fields[1]
+		tag=fields[2]
+		print "%s\t%s" & (movieId,tag)
+	
+	except Exception:
+		continue
